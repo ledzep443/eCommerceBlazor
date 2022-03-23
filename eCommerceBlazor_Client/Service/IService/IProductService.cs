bb888810b16db875ed7dac872d58c26709667bc1
Jimmy Purnell <jimmypurnell27@gmail.com>
2022-03-23 18:08:40 -0400

    Added product service interface

---
+++ eCommerceBlazor_Client/Service/IService/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using eCommerceBlazor_Models;
+
+namespace eCommerceBlazor_Client.Service.IService
+{
+    public interface IProductService
+    {
+        public Task<IEnumerable<ProductDTO>> GetAll();
+        public Task<ProductDTO> Get(int productId);
+    }
+}

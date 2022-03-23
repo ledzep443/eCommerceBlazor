02b5214346d9bd20c969d9ce98c88c2aca564775
Jimmy Purnell <jimmypurnell27@gmail.com>
2022-03-23 18:04:19 -0400

    Added helpers and  authentication services

---
+++ eCommerceBlazor_Client/Service/AuthStateProvider.cs
@@ -0,0 +1,45 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using eCommerceBlazor_Common;
+using eCommerceBlazor_Client.Helper;
+
+namespace eCommerceBlazor_Client.Service
+{
+    public class AuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILocalStorageService _localStorageService;
+
+        public AuthStateProvider(HttpClient httpClient, ILocalStorageService localStorageService)
+        {
+            _httpClient = httpClient;
+            _localStorageService = localStorageService;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var token = await _localStorageService.GetItemAsync<string>(SD.Local_Token);
+            if (token == null)
+            {
+                return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+            }
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", token);
+            return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(JwtParser.ParseClaimsFromJwt(token), "jwtAuthType")));
+        }
+
+        public void NotifyUserLoggedIn(string token)
+        {
+            var authenticatedUser = new ClaimsPrincipal(new ClaimsIdentity(JwtParser.ParseClaimsFromJwt(token), "jwtAuthType"));
+            var authState = Task.FromResult(new AuthenticationState(authenticatedUser));
+            NotifyAuthenticationStateChanged(authState);
+        }
+
+        public void NotifyUserLogout()
+        {
+            var authState = Task.FromResult(new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity())));
+            NotifyAuthenticationStateChanged(authState);
+        }
+    }
+}

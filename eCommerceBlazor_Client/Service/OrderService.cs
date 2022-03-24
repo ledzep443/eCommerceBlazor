83a377f341c6fedbdad79df5ae35890459e93793
Jimmy Purnell <jimmypurnell27@gmail.com>
2022-03-23 20:53:40 -0400

    Completed order service and interface

---
+++ eCommerceBlazor_Client/Service/OrderService.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+using eCommerceBlazor_Models;
+using eCommerceBlazor_Client.Service.IService;
+
+namespace eCommerceBlazor_Client.Service
+{
+    public class OrderService : IOrderService
+    {
+        private readonly HttpClient _httpClient;
+        private IConfiguration _configuration;
+        private string BaseServerUrl;
+        public OrderService(HttpClient client, IConfiguration configuration)
+        {
+            _httpClient = client;
+            _configuration = configuration;
+            BaseServerUrl = _configuration.GetSection("BaseServerUrl").Value;
+        }
+
+        public async Task<OrderDTO> Create(StripePaymentDTO stripePayment)
+        {
+            var content = JsonConvert.SerializeObject(stripePayment);
+            var bodyContent = new StringContent(content, Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync("api/order/create", bodyContent);
+            string responseResult = response.Content.ReadAsStringAsync().Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var result = JsonConvert.DeserializeObject<OrderDTO>(responseResult);
+                return result;
+            }
+            return new OrderDTO();
+        }
+
+        public async Task<OrderDTO> Get(int? orderHeaderId)
+        {
+            var response = await _httpClient.GetAsync($"api/order{orderHeaderId}");
+            var content = await response.Content.ReadAsStringAsync();
+            if (response.IsSuccessStatusCode)
+            {
+                var order = JsonConvert.DeserializeObject<OrderDTO>(content);
+                return order;
+            }
+            else
+            {
+                var errorModel = JsonConvert.DeserializeObject<ErrorModelDTO>(content);
+                throw new Exception(errorModel.ErrorMessage);
+            }
+        }
+
+        public async Task<IEnumerable<OrderDTO>> GetAll(string? userId = null)
+        {
+            var response = await _httpClient.GetAsync("api/order");
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var orders = JsonConvert.DeserializeObject<IEnumerable<OrderDTO>>(content);
+
+                return orders;
+            }
+            return new List<OrderDTO>();
+        }
+
+        public async Task<OrderHeaderDTO> MarkPaymentSuccessful(OrderHeaderDTO orderHeader)
+        {
+            var content = JsonConvert.SerializeObject(orderHeader);
+            var bodyContent = new StringContent(content, Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync("api/order/paymentsuccessful", bodyContent);
+            string responseResult = response.Content.ReadAsStringAsync().Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var result = JsonConvert.DeserializeObject<OrderHeaderDTO>(responseResult);
+                return result;
+            }
+
+            else
+            {
+                var errorModel = JsonConvert.DeserializeObject<ErrorModelDTO>(responseResult);
+                throw new Exception(errorModel.ErrorMessage);
+            }
+        }
+    }
+}

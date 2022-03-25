0e86a60883acea8060d18b8ce9590d194be203b7
Jimmy Purnell <jimmypurnell27@gmail.com>
2022-03-25 01:46:03 -0400

    Admin webserver deploy settings

--- eCommerceBlazor_Client/wwwroot/js/StripePayment.js
+++ eCommerceBlazor_Client/wwwroot/js/StripePayment.js
@@ -1,5 +1,5 @@
 ﻿redirectToCheckout = function (sessionId) {
-    var stripe = Stripe("");
+    var stripe = Stripe("pk_test_51Kh47pL6lk7SigOZVbajneG7rUTyfQycyUx4iWZ3AJum7iqsIbY20khdyvn4UxUpwfmnwVSdD8BXsP3wQxS5CJdI00vydotMfg");
     stripe.redirectToCheckout({
         sessionId: sessionId
     });

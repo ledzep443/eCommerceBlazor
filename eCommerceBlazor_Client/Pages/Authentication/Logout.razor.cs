1aa7af1ce049733080302dd37f962b765652db0f
Jimmy Purnell <jimmypurnell27@gmail.com>
2022-03-24 23:06:50 -0400

    Added logout page

---
+++ eCommerceBlazor_Client/Pages/Authentication/Logout.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using eCommerceBlazor_Client.Service.IService;
+
+namespace eCommerceBlazor_Client.Pages.Authentication
+{
+    public partial class Logout
+    {
+        [Inject]
+        public IAuthenticationService _authService { get; set; }
+        [Inject]
+        public NavigationManager _navigationManager { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            await _authService.Logout();
+            _navigationManager.NavigateTo("/");
+        }
+    }
+}
